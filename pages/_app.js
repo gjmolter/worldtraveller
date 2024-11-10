c37346f156b51fb897eed8729449aae6bf16a546
Gabriel Molter <gabriel@chillycapybara.com>
2024-11-09 21:09:39 -0800

    chore: add plausible analytics

--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,7 @@ function MyApp({ Component, pageProps }) {
           rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
         />
+        <script defer data-domain="worldtraveller.vercel.app" src="https://analytics.cpbr.digital/js/script.js"></script>
       </Head>
       <Component {...pageProps} />
     </>
